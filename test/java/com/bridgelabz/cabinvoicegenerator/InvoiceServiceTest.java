026f452a76cf380bfde274c1080dfbd56d6276cf
sharale34 <sourabhharale8000@gmail.com>
2020-10-22 10:59:51 +0530

    Added: Ability to return the total no.of rides, total fare, average fare per ride

--- test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
 
+	InvoiceGenerator invoiceGenerator = null;
+
+	@Before
+	public void setUp() throws Exception {
+		invoiceGenerator = new InvoiceGenerator();
+	}
+
 	@Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -16,7 +23,6 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 2;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -24,10 +30,10 @@ public class InvoiceServiceTest {
 	}
 
 	@Test
-	public void givenMultipleRidesShouldReturnAggregateTotalForAll() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+	public void givenMultipleRidesShouldReturnInvoiceSummary() {
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-		double totalFare = invoiceGenerator.calculateFare(rides);
-		Assert.assertEquals(30, totalFare, 0.0);
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
 	}
 }
